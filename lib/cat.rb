78438bce80fdc2f5e82c020ad579152c84ecee58
tyler <tylercharawell@gmail.com>
2021-07-06 11:20:59 -0700

    commit tree 83cff7da2817d34c013ea646a3e91554884d8831

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,26 @@
+require 'pry'
 class Cat
-  # code goes here
+  
+@@all = []
+
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+def initialize(name, owner)
+  @owner = owner
+  @name = name
+  @mood = "nervous"
+  @@all << self
+end
+
+def name
+  @name
+end
+
+def self.all
+  @@all
+end
+
+# binding.pry
+
 end
